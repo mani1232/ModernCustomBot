94a0c455ae81886ff4b31f789463d71b50d2f3b3
mani1232 <perederkoura@gmail.com>
2023-11-19 03:17:43 +0100

    upd

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.9.20"
-    kotlin("plugin.serialization") version "1.9.20"
+    kotlin("jvm") version "2.0.0-Beta1"
+    kotlin("plugin.serialization") version "2.0.0-Beta1"
     application
 }
 
@@ -14,7 +14,7 @@ repositories {
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.7.3")
     implementation("com.charleskorn.kaml:kaml-jvm:0.55.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.6.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.6.1")
     implementation("ch.qos.logback:logback-classic:1.4.11")
     implementation("net.dv8tion:JDA:5.0.0-beta.17") {
         exclude("opus-java")
