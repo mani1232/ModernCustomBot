d481c827c28f966267c0cf9d9f1ca15bfa314717
mani1232 <perederkoura@gmail.com>
2023-12-11 19:42:35 +0100

    add repo for api

--- mcb-api/build.gradle.kts
+++ mcb-api/build.gradle.kts
@@ -1,3 +1,7 @@
+plugins {
+    `maven-publish`
+}
+
 val productName = findProperty("product-name")!! as String
 
 dependencies {
@@ -7,5 +11,26 @@ dependencies {
 tasks {
     shadowJar {
         archiveFileName = "$productName-$version-api.jar"
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "WorldMandiaRepository"
+            url = uri("https://repo.worldmandia.cc/releases")
+            credentials(PasswordCredentials::class)
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "cc.worldmandia"
+            artifactId = "mcb-api"
+            version = project.version.toString()
+            from(components["java"])
+        }
     }
 }
